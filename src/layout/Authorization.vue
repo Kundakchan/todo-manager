167585a0afc5639c1f60f773108db6a19436e196
kundakchan <kundakchan@yandex.ru>
2023-06-13 21:14:11 +0300

    [1] - add layout registrations

--- src/layout/Authorization.vue
+++ src/layout/Authorization.vue
@@ -2,6 +2,27 @@
 </script>
 
 <template>
-  <h2>Authentication layout</h2>
-  <router-view />
+  <a-row>
+    <a-col class="background-container" :span="17">
+      <img class="background" src="@/assets/images/authorization/background.png" alt="">
+    </a-col>
+    <a-col class="slot" :span="7">
+      <router-view />
+    </a-col>
+  </a-row>
 </template>
+
+<style lang="scss" scoped>
+.background-container {
+  height: 100vh;
+  overflow: hidden;
+  clip-path: polygon(0 0, 100% 0, 100% 0, 40% 100%, 0 100%);
+}
+.background {
+  height: 100%;
+  float: right;
+}
+.slot {
+  padding: 24px;
+}
+</style>
