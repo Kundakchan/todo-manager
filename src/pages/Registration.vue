8f1990d2aaafde0183bfe16fd9c91fed1419612b
kundakchan <kundakchan@yandex.ru>
2023-06-14 13:14:39 +0300

    [5] - add registration component

--- src/pages/Registration.vue
+++ src/pages/Registration.vue
@@ -3,5 +3,27 @@
 </script>
 
 <template>
-  <h1>Registration page</h1>
+  <div class="login">
+    <a-typography-title :level="3">Регистрация</a-typography-title>
+    <a-typography-title :level="5">Имя пользователя</a-typography-title>
+    <a-input />
+    <a-typography-title :level="5">Email</a-typography-title>
+    <a-input />
+    <a-typography-title :level="5">Пароль</a-typography-title>
+    <a-input />
+    <a-row class="action-panel" :gutter="16" align="middle" justify="space-between">
+      <a-col>
+        <a-button type='primary'>Регистрация</a-button>
+      </a-col>
+      <a-col>
+        <router-link :to="{ name: 'Login' }">Авторизоваться</router-link>
+      </a-col>
+    </a-row>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.action-panel {
+  margin-top: 24px;
+}
+</style>
